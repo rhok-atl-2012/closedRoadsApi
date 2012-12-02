9a1dc969c1025051138695cee68dcdbc81d639a4
Richard Rissanen <richardrissanen@me.com>
2012-12-02 11:22:40 -0500

    playing with headers syntax

--- routes/api.rb
+++ routes/api.rb
@@ -19,18 +19,17 @@ helpers do
   end
 end
 
-get '/new/event/:name/:start_date/:end_date/:street_ids/?' do
-	headers 'Content-Type' => "text/html;charset=utf-8"
+get '/new/event/:name/:start_date/:end_date/:street_ids/?' do 
 	arr = params[:street_ids].split("+")
 	streets = []
 	arr.each {|str| streets << str.to_f }
 	event = Event.create(name: params[:name], start_date: params[:start_date], end_date: params[:end_date], street_ids: streets)
 	event.saved? ? response = 'Event succesfully created.' : response = 'Event creation failed.'
+	[200, 'Content-Type' => "text/javascript;charset=utf-8"]
 	return response.to_json	
 end
 
 get '/events/:start_date/:end_date/:center_long/:center_lat/:radius/?' do
-		headers 'Content-Type' => "text/html;charset=utf-8"
 		events = Event.all(:start_date.gt => params[:start_date], :end_date.lt => params[:end_date])
 		results =[]
 		unless events.nil? || events.empty?
@@ -55,6 +54,7 @@ get '/events/:start_date/:end_date/:center_long/:center_lat/:radius/?' do
 				end
 			end
 			og = results.uniq!
+			[200, 'Content-Type' => "text/javascript;charset=utf-8"]
 			return results.to_json
 		end
 end
